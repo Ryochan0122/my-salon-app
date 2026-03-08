11a1df8d3ade1a06165531b9cf915951833c22ea
Me <ryota.k.612@icloud.com>
2026-03-08 09:22:30 +0900

    fix: auth reload fix

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,16 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    auth: {
-      persistSession: true,
-      storageKey: 'aura-auth',
-      storage: window?.localStorage,
-      autoRefreshToken: true,
-      detectSessionInUrl: false,
-    }
-  }
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
 export const getCurrentShopId = async () => {
